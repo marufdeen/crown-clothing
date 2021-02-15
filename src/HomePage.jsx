b6928d00b436eecc0f01fe06291ea02b61d796db
Maruf Ajagunna <marufajagunna@gmail.com>
2021-02-15 18:39:05 +0100

    adding sass file to homepage

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -1,33 +1,39 @@
 import React from "react";
-
+import "./homepage.scss";
 const HomePage = () => {
   return (
     <div className="homepage">
       <div className="directory-menu">
         <div className="menu-item">
           <div className="content">
-            <h1 className="title">HATs</h1>
+            <h1 className="title">HATS</h1>
             <span className="subtitle">SHOP NOW</span>
           </div>
         </div>
         <div className="menu-item">
-        <div className="content">
-          <h1 className="title">HATs</h1>
-          <span className="subtitle">SHOP NOW</span>
+          <div className="content">
+            <h1 className="title">JACKETS</h1>
+            <span className="subtitle">SHOP NOW</span>
+          </div>
         </div>
-      </div>
-      <div className="menu-item">
-      <div className="content">
-        <h1 className="title">HATs</h1>
-        <span className="subtitle">SHOP NOW</span>
-      </div>
-    </div>
-    <div className="menu-item">
-    <div className="content">
-      <h1 className="title">HATs</h1>
-      <span className="subtitle">SHOP NOW</span>
-    </div>
-  </div>
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">SNICKERS</h1>
+            <span className="subtitle">SHOP NOW</span>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">WOMENS</h1>
+            <span className="subtitle">SHOP NOW</span>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">MENS</h1>
+            <span className="subtitle">SHOP NOW</span>
+          </div>
+        </div>
       </div>
     </div>
   );
