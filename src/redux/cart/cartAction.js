2072337894d171f59fe37e745594dbf323f220a2
Maruf Ajagunna <marufajagunna@gmail.com>
2021-02-21 17:10:43 +0100

    updated shop data with new data, implemented cartUtils to group items in cartReducer

--- src/redux/cart/cartAction.js
+++ src/redux/cart/cartAction.js
@@ -1,5 +1,9 @@
-import { cartActionTypes } from './cartActionTypes';
-
+import { cartActionTypes } from './cartActionTypes'; 
 export const toggleCartHidden = () => ({
     type: cartActionTypes.TOGGLE_CART_HIDDEN
-})
+});
+
+ export const addItem = (item) => ({
+    type: cartActionTypes.ADD_ITEM,
+    payload: item
+}) 
